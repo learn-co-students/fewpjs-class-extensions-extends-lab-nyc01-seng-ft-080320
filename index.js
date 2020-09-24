0cf477e3801fa776f1bf0119cf5d28a29d6e99e0
royFloresNyc <royfloresnyc@gmail.com>
2020-09-24 12:31:17 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ class Polygon {
             return sum + num
         })
     }
+}
 
+class Triangle extends Polygon {
+    get isValid(){
+        return ((this.sides[0] + this.sides[1] > this.sides[2]) &&
+            (this.sides[0] + this.sides[2] > this.sides[1]) &&
+            (this.sides[1] + this.sides[2] > this.sides[0]))
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        return this.sides.every(side => side === this.sides[0])
+    }
+
+    get area() {
+        return this.sides[0] ** 2
+    }
 }
