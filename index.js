141166c0db48832acc2a5784b6bd4e306bb05500
royFloresNyc <royfloresnyc@gmail.com>
2020-09-24 10:12:58 -0400

    Polygon class with getter methods

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+class Polygon {
+    constructor (arrayOfInt){
+        this.sides = arrayOfInt
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        return this.sides.reduce( (sum, num ) => {
+            return sum + num
+        })
+    }
+
+}
